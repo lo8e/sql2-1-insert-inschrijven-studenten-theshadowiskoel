0a90831bcf365571030d42f27cc70b0b1ee1e84c
MidnightBeast <jobboy209@gmail.com>
2019-05-10 15:10:06 +0200

    Studenten Klassen gekoppeld
    ik heb de studenten aan de klassen gekoppeld

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -9,3 +9,10 @@ INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboorted
 	(90002,'Matthijs','','Verboon','2002-07-22','2225 GA','Katwijk aan Zee', 67.0, 1.82, '2019-01-01'),
     (90003,'Matthijs','','Harren','2000-08-03','2225 GB','Leiden', 67.0, 1.82, '2019-01-01'),
     (90004,'Spek','van der','Rick','2000-12-03','2225 CE','Leiden', 67.0, 1.82, '2019-01-01');
+    
+INSERT INTO klassen_studenten(ov_nummer, klas_code) VALUES 
+	(90001, 'lo9e-amo2'),
+	(90002, 'lo9e-amo1'),
+	(90003, 'lo9e-amo3'),
+	(90004, 'lo9e-amo2');
+    
