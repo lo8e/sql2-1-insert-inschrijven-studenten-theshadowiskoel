8f4b0fc4df70744b94e26e90898aa321384619d2
MidnightBeast <jobboy209@gmail.com>
2019-05-10 14:53:41 +0200

    Studenten toegevoegd
    3 studenten toegevoegd aan de studenten tabel.

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -4,4 +4,7 @@ INSERT INTO klassen
 	('lo9e-amo2','vlt03', 2019, 'amo'), 
     ('lo9e-amo3','rs001', 2018, 'amo');
     
-
+INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum) VALUES
+	(90001,'Dijk','van','Job','2002-08-03','2225 ZE','Katwijk aan Zee', 67.0, 1.82, '2019-01-01'),
+	(90002,'Matthijs','','Verboon','2002-07-22','2225 GA','Katwijk aan Zee', 67.0, 1.82, '2019-01-01'),
+    (90003,'Matthijs','','Harren','2000-08-03','2225 GB','Leiden', 67.0, 1.82, '2019-01-01');
