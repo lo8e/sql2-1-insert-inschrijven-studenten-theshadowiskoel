ab22cf3c3a53974bbb4889f6bddfc3900711c03d
MidnightBeast <jobboy209@gmail.com>
2019-05-10 14:59:14 +0200

    1 student vergeten

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -7,4 +7,5 @@ INSERT INTO klassen
 INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum) VALUES
 	(90001,'Dijk','van','Job','2002-08-03','2225 ZE','Katwijk aan Zee', 67.0, 1.82, '2019-01-01'),
 	(90002,'Matthijs','','Verboon','2002-07-22','2225 GA','Katwijk aan Zee', 67.0, 1.82, '2019-01-01'),
-    (90003,'Matthijs','','Harren','2000-08-03','2225 GB','Leiden', 67.0, 1.82, '2019-01-01');
+    (90003,'Matthijs','','Harren','2000-08-03','2225 GB','Leiden', 67.0, 1.82, '2019-01-01'),
+    (90004,'Spek','van der','Rick','2000-12-03','2225 CE','Leiden', 67.0, 1.82, '2019-01-01');
